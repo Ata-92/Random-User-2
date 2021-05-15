d5e8839e586641ff959b51cf018e8e120b9f9d64
Ata-92 <atakal92@gmail.com>
2021-05-15 16:44:48 +0300

    Import images, axios,useState and UseEffect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import cw from "./assets/cw.svg";
+import man from "./assets/man.svg";
+import woman from "./assets/woman.svg";
+import email from "./assets/mail.svg";
+import oldman from "./assets/growing-up-man.svg";
+import oldwoman from "./assets/growing-up-woman.svg";
+import location from "./assets/map.svg";
+import phone from "./assets/phone.svg";
+import password from "./assets/padlock.svg";
+import design from "./assets/design.svg";
+
 function App() {
   return (
     <div className="App">
